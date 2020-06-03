7c74625bb3b4e75a9b95bc0778d420e021df3cf2
Paul Stack <public@paulstack.co.uk>
2020-06-03 17:51:16 +0100

    Migrate to GHA for builds (#7)
    * Migrate to GHA for builds
    
    * Adding the generation of the Go SDK to ensure it is correct

---
+++ examples/examples_py_test.go
@@ -0,0 +1,32 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// +build python all
+
+package examples
+
+import (
+	"path"
+	"path/filepath"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
+
+func TestAccProjectPython(t *testing.T) {
+	test := getPythonBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "project", "python"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getPythonBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions(t)
+	basePy := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			filepath.Join("..", "sdk", "python", "bin"),
+		},
+	})
+
+	return basePy
+}
