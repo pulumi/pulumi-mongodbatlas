5027dc1b5bddbfca1bcf3e5a0265981ac39f91bc
Venelin <venelin@pulumi.com>
2024-03-29 17:02:58 +0000

    fix upstream provider call

--- provider/shim/shim.go
+++ provider/shim/shim.go
@@ -7,6 +7,6 @@ import (
 	p "github.com/mongodb/terraform-provider-mongodbatlas/internal/provider"
 )
 
-func FrameworkProvider() provider.Provider { return p.NewFrameworkProvider() }
+func FrameworkProvider() provider.Provider { return p.NewFrameworkProvider(nil) }
 
-func SDKProvider() *schema.Provider { return p.NewSdkV2Provider() }
+func SDKProvider() *schema.Provider { return p.NewSdkV2Provider(nil) }
