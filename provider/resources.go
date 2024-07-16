f722e3d861592cdc14e45354d8c81204666b4229
VenelinMartinov <venelin.v.martinov@gmail.com>
2024-07-16 14:31:27 +0300

    Enable PlanResourceChange by default (#532)
    Part of https://github.com/pulumi/pulumi-terraform-bridge/issues/1785
    
    Enable the bridge feature for mongodbatlas

--- provider/resources.go
+++ provider/resources.go
@@ -69,7 +69,8 @@ func Provider() tfbridge.ProviderInfo {
 	ctx := context.Background()
 	// Instantiate the Terraform provider
 	p := pfbridge.MuxShimWithPF(ctx,
-		shimv2.NewProvider(mongodbatlas.SDKProvider()),
+		shimv2.NewProvider(mongodbatlas.SDKProvider(),
+			shimv2.WithPlanResourceChange(func(string) bool { return true })),
 		mongodbatlas.FrameworkProvider())
 
 	// Create a Pulumi provider mapping
